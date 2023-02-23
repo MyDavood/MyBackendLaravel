2cdbea564b5d929db4f88c0d532dd82d2c9f4f73
Davood Jafari <to.jafari@gmail.com>
2023-02-23 03:56:10 +0330

    2023-02-23 03-03

--- src/Http/Controllers/BaseController.php
+++ src/Http/Controllers/BaseController.php
@@ -4,18 +4,31 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Inertia\Response;
 
 class BaseController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function inertiaDataTable($component, $dataTableClass, array $params = [])
+    public function inertiaDataTable(
+        string $component,
+        $dataTableClass,
+        array $params = [],
+        string $formComponent = null,
+        mixed $formItem = null,
+    ): Response
     {
         $dataTable = new $dataTableClass;
+
+        if ($formComponent != null) {
+            inertia()->share(['formComponent' => $formComponent]);
+        }
 
         return inertia($component, [
             'searchFields' => fn() => $dataTable->searchFields(),
             'dataTable' => fn() => $dataTable,
+            'form' => fn() => $formComponent != null,
+            'formItem' => fn() => $formItem,
             ...$params,
         ]);
     }
